054aae18c007bc0f3a039d5bcfccfa7c7fbe94db
Juho Vepsalainen <bebraw@gmail.com>
2015-02-12 16:59:54 +0200

    Add stub for Client table

--- app/Registers/Clients/index.jsx
+++ app/Registers/Clients/index.jsx
@@ -1,12 +1,14 @@
 'use strict';
 var React = require('react');
 var Reflux = require('reflux');
+var Table = require('reactabular').Table;
 
 
 module.exports = function(api) {
     var clientActions = require('./actions')(api);
     var clientStore = require('./store')(clientActions);
 
+    // TODO: get client schema and convert to column definition
     return React.createClass({
         mixins: [Reflux.connect(clientStore, 'clients')],
 
@@ -19,10 +21,46 @@ module.exports = function(api) {
         render: function() {
             var clients = this.state.clients || [];
 
+            var data = [
+                {
+                    name: 'React.js',
+                    type: 'library',
+                    description: 'Awesome library for handling view.',
+                },
+                {
+                    name: 'Angular.js',
+                    type: 'framework',
+                    description: 'Swiss-knife of frameworks. Kitchen sink not included.',
+                },
+                {
+                    name: 'Aurelia',
+                    type: 'framework',
+                    description: 'Framework for the next generation',
+                },
+            ];
+
+            var columns = [
+                {
+                    property: 'name',
+                    header: 'Name',
+                },
+                {
+                    property: 'type',
+                    header: 'Type',
+                },
+                {
+                    property: 'description',
+                    header: 'Description',
+                },
+            ];
+
             return <div>
                 <h2>Clients</h2>
 
-                <p>This should show clients</p>
+                <Table
+                    className='pure-table pure-table-striped'
+                    columns={columns}
+                    data={data}></Table>
             </div>;
         }
     });
