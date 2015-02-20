6f8e01bb185a3d9bf9bda12c2f6675e1e6bc3cbc
Juho Vepsalainen <bebraw@gmail.com>
2015-02-20 09:28:12 +0200

    Allow development server to be accessed from any url
    Just needed to set `historyApiFallback` as `true`.

--- dev-server.js
+++ dev-server.js
@@ -10,6 +10,7 @@ var ip = '0.0.0.0';
 new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     hot: true,
+    historyApiFallback: true,
 }).listen(port, ip, function (err) {
     if(err) {
         return console.log(err);
