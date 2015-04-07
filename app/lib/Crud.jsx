cd3432cf4c7738fe3773b6de91d58c4b45f3e87e
Juho Vepsalainen <bebraw@gmail.com>
2015-04-07 14:16:20 +0300

    Generalize Crud views

---
+++ app/lib/Crud.jsx
@@ -0,0 +1,45 @@
+'use strict';
+var React = require('react');
+var titleCase = require('title-case');
+
+var Create = require('./Create.jsx');
+var Table = require('./Table.jsx');
+var getSchema = require('./get_schema');
+
+
+module.exports = function(name) {
+    return function(api) {
+        var multipleName = name + 's';
+        var upperMultipleName = titleCase(multipleName);
+        var actions = require('./Actions')(api, multipleName);
+        var store = require('./Store')(actions);
+        var schema = getSchema(api[multipleName]);
+        var createNew = 'Create a new ' + name;
+
+        return React.createClass({
+            displayName: upperMultipleName,
+
+            render: function() {
+                return (
+                    <div className='pure-g'>
+                        <div className='pure-u-1'>
+                            <h2>{upperMultipleName}</h2>
+                        </div>
+
+                        <div className='pure-u-1 controls'>
+                            <Create schema={schema} actions={actions}>
+                                {createNew}
+                            </Create>
+                        </div>
+
+                        <div className='pure-u-1'>
+                            <Table
+                                store={store} actions={actions}
+                                schema={schema} />
+                        </div>
+                    </div>
+                );
+            },
+        });
+    };
+};
